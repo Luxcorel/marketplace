6bf1e01f7c551fe062eae6a2bdd91355016a36bf
Johannes Rosengren <98045373+Luxcorel@users.noreply.github.com>
2024-06-27 11:00:05 +0200

    Add docker compose

---
+++ db-init/02-init.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.product_category (id, name) VALUES ('d5509745-450f-4760-8bdd-ddc88d376b37', 'cars');
+INSERT INTO public.product_category (id, name) VALUES ('a84f84bf-ddbc-4718-a1a5-1f80f938133b', 'electronics');
+INSERT INTO public.product_category (id, name) VALUES ('60be22b6-b1c0-4d90-a98e-c4d7726c71f7', 'kebab');
+INSERT INTO public.product_category (id, name) VALUES ('f567cf44-3aff-4a95-9169-7b5cc748a1cc', 'phones');
+INSERT INTO public.product_category (id, name) VALUES ('4418f2af-41d1-4b26-b294-61977a824cde', 'furniture');
+INSERT INTO public.account (id, first_name, last_name, date_of_birth, email, password, username) VALUES ('bea17a26-1220-4bce-ac86-9a9b85f7ab26', 'John', 'Doe', '2000-01-01', 'john@example.org', '$2a$10$WkC9jLaZFwdaqGnw2w.Cg.fUnnyqedOyLEjdtrFUhosj2ahA4DXkq', 'john');
